ed43cd104854ff2761343be535746c0cb15f4687
Aytek <aytekekici@gmail.com>
2014-01-08 22:56:48 +0200

    fixed expirytime

--- AspNet.Caching.Output/AspNet.Caching.Output/Providers/RedisOutputCachingProvider.cs
+++ AspNet.Caching.Output/AspNet.Caching.Output/Providers/RedisOutputCachingProvider.cs
@@ -79,7 +79,7 @@ namespace AspNet.Caching.Output.Providers
                 bool lockTaken = redisConnection.Wait(lockTakenTask);
                 if (lockTaken)
                 {
-                    Task setTask = redisConnection.Strings.Set(1, key, raw);
+                    Task setTask = redisConnection.Strings.Set(1, key, raw, (DateTime.UtcNow - utcExpiry).Seconds);
                     Task releaseLockTask = redisConnection.Strings.ReleaseLock(1, "lock:" + key);
                     redisConnection.WaitAll(new[] { setTask, releaseLockTask });
                     break;
